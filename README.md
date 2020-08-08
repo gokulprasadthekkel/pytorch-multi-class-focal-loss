0c942eed9ec002fdcc26b73f37c210cb4f646c94
Gokul Prasad <gokulprasadthekkel@gmail.com>
2020-08-08 12:23:10 +0530

    initial commit

--- README.md
+++ README.md
@@ -1,1 +1,11 @@
 # pytorch-multi-class-focal-loss
+
+An implementation of multi-class focal loss in pytorch.<br>
+
+Focal loss,originally developed for handling extreme foreground-background class imbalance in object detection algorithms, could be used as an alternative for cross-entropy loss when you have imbalanced datasets.
+<br>original paper: https://arxiv.org/abs/1708.02002
+<br>
+
+![Equation](images/equation.jpg)
+
+
---
+++ focal_loss.py
@@ -0,0 +1,16 @@
+import torch
+import torch.nn as nn
+import torch.nn.functional as F
+
+class FocalLoss(nn.modules.loss._WeightedLoss):
+    def __init__(self, weight=None, gamma=2,reduction='mean'):
+        super(FocalLoss, self).__init__(weight,reduction=reduction)
+        self.gamma = gamma
+        self.weight = weight #weight parameter will act as the alpha parameter to balance class weights
+
+    def forward(self, input, target):
+
+        ce_loss = F.cross_entropy(input, target,reduction=self.reduction,weight=self.weight)
+        pt = torch.exp(-ce_loss)
+        focal_loss = ((1 - pt) ** self.gamma * ce_loss).mean()
+        return focal_loss
